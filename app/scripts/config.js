f1be54840b5514b40109ca0e2374a089e91cb288
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-02-25 20:24:35 +0100

    feature(config): add bower dependencies
    setup some dependencies using bower and configure path + shim for
    requirejs

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -13,6 +13,22 @@ require.config({
       ],
       exports: 'Backbone'
     },
+    marionette: {
+      deps: ['backbone', 'backbone.babysitter', 'backbone.wreqr'],
+      exports: 'Backbone.Marionette'
+    },
+    'backbone.babysitter': {
+      deps: ['underscore', 'backbone'],
+      exports: 'Backbone.BabySitter'
+    },
+    'backbone.wreqr': {
+      deps: ['underscore', 'backbone'],
+      exports: 'Backbone.Wreqr'
+    },
+    'backbone.syphon':  {
+      deps: ['backbone'],
+      exports: 'Backbone'
+    },
     handlebars: {
       exports: 'Handlebars'
     }
@@ -21,7 +37,12 @@ require.config({
     jquery: '../bower_components/jquery/jquery',
     backbone: '../bower_components/backbone/backbone',
     underscore: '../bower_components/underscore/underscore',
-    handlebars: '../bower_components/handlebars/handlebars.runtime'
+    handlebars: '../bower_components/handlebars/handlebars.runtime',
+    marionette: '../bower_components/marionette/lib/core/amd/backbone.marionette',
+    'backbone.babysitter': '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+    'backbone.wreqr': '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
+    'backbone.syphon': '../bower_components/backbone.syphon/lib/amd/backbone.syphon',
+    superagent: '../bower_components/superagent/superagent'
   },
 
   deps: ['jquery', 'underscore']
