da168666cc543bae10509f4761e76f9c725b5ec2
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-05-06 12:18:05 +0200

    chore(bower): inject backbone.syphon dep

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -29,6 +29,7 @@ require.config({
     marionette: '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
+    syphon: '../bower_components/backbone.syphon/lib/amd/backbone.syphon',
     superagent: '../bower_components/superagent/superagent',
     fastclick: '../bower_components/fastclick/lib/fastclick'
   },
