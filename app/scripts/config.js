1a2e4f10f8e3024fe0cabb4b3133d517f2ec64a1
Maxime Tyler <127086@supinfo.com>
2014-05-26 17:48:12 +0200

    feat(i18n): add discover of browser locale

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var locale;
+
 require.config({
   shim: {
     // zepto
@@ -28,7 +30,34 @@ require.config({
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     syphon: '../bower_components/backbone.syphon/lib/amd/backbone.syphon',
     superagent: '../bower_components/superagent/superagent',
+    text: '../bower_components/requirejs-text/text',
+    json: '../bower_components/requirejs-plugins/src/json',
+    replace: '../bower_components/require.replace/require.replace',
     fastclick: '../bower_components/fastclick/lib/fastclick'
+  },
+
+  config: {
+    replace: {
+      // Replace `nls` with the locale of the navigator
+      // It will be used to find the right `i18n.json` translation file.
+      pattern: 'nls',
+      value: function () {
+        if (locale) {
+          return locale;
+        }
+        var parts = window.location.search.match(/([^?=&]+)(=([^&]*))?/g);
+        for (var i in parts) {
+          var tokens = parts[i].split('=');
+          if (tokens[0] === 'lang') {
+            locale = tokens[1];
+            return locale;
+          }
+        }
+        var language = window.navigator.language || window.navigator.userLanguage;
+        locale = language.substr(0, 2) || 'en';
+        return locale;
+      }
+    }
   },
 
   deps: ['jquery', 'underscore']
